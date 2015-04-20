cc1edd0d124488bd68c6fcb99b70ff0aaddc273c
dougdougs <dsilva@sumo.do>
2015-04-20 15:32:21 -0300

    Start project: Sass + Bootstrap OK

--- lifestyleBlog/wordpress/wp-content/themes/custom-template/header.php
+++ lifestyleBlog/wordpress/wp-content/themes/custom-template/header.php
@@ -1,50 +1,79 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+	<link href="//www.google-analytics.com" rel="dns-prefetch">
+	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php bloginfo('description'); ?>">
 
-	</head>
-	<body <?php body_class(); ?>>
+	<?php wp_head(); ?>
+	<script>
+		// conditionizr.com
+		// configure environment tests
+		conditionizr.config({
+			assets: '<?php echo get_template_directory_uri(); ?>',
+			tests: {}
+		});
+	</script>
 
-		<!-- wrapper -->
-		<div class="wrapper">
 
-			<!-- header -->
-			<header class="header clear" role="banner">
+	<!-- css -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/global.css" >
+	<!-- css -->
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
+</head>
+<body <?php body_class(); ?>>
+
+	<!-- wrapper -->
+	<div class="wrapper-blog">
+
+		<!-- header -->
+		<header class="header clear" role="banner">
+			<!-- nav -->
+			<!--
+				<nav class="nav">
+					<?php html5blank_nav(); ?>
+				</nav> 
+			-->
+			<nav class="nav navbar navbar-default"  role="navigation">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- logo -->
+						<a class="navbar-brand logo" href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
 						</a>
+						<!-- /logo -->
+
 					</div>
-					<!-- /logo -->
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<?php wp_nav_menu( 
+							array( 
+								'theme_location' => 'header-menu', 
+								'items_wrap' => '<ul class="nav navbar-nav">%3$s</ul>' 
+							) 
+						); ?>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+			<!-- /nav -->
 
-			</header>
-			<!-- /header -->
+		</header>
+		<!-- /header -->
